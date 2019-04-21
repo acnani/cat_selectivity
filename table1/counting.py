384dac95b2c60231253af93328b03bdf8cea4fa5
Ameya <acnani@gmail.com>
2019-04-20 22:54:58 -0400

    script to generate binary search summary table

---
+++ table1/counting.py
@@ -0,0 +1,19 @@
+import helperFcns as hf
+import numpy as np
+import pandas as pd
+
+eType = 'epineural'
+subjectList = hf.getSubjects(eType)
+
+thresholdRes = {}
+for iSub in subjectList:
+    print iSub
+    thresholdRes[iSub] = hf.getBinarySearchParams(iSub,eType)
+DF = pd.DataFrame.from_dict(thresholdRes).transpose()
+tmp = pd.read_csv('..\\figure2\\selective_counts.csv',index_col=0)
+
+binarySummary = pd.concat([DF,tmp],1)
+binarySummary.to_csv('binarySearch_summary.csv')
+
+print binarySummary
+
