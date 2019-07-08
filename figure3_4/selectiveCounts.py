06886c154b5bbae96661678607288cd8865cd389
Ameya <acnani@gmail.com>
2019-07-08 14:11:24 -0400

    separated coactivation matrix snippet and added save for selectiveDF dataframe

--- figure3_4/selectiveCounts.py
+++ figure3_4/selectiveCounts.py
@@ -3,8 +3,10 @@ from helperFcns import plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import plotly.graph_objs as go
+import plotly
 
-eType = 'penetrating'
+eType = 'epineural'
 
 collapseCuffs = True
 ignoreCuffs = ['BiFem']
@@ -15,16 +17,13 @@ colorList = hf.colorOrder
 
 subjectList = hf.getSubjects(eType)
 targetNerveLabels = hf.allCuffs_mdf.keys()
-coactivationDF = pd.DataFrame(0,columns=targetNerveLabels, index=targetNerveLabels)
 
-selectiveDF = pd.DataFrame(columns=['DRG', 'nerve', 'subject','Threshold','Dynamic Range','Threshold (nC)','Dynamic Range (nC)','type'])
+selectiveDF = pd.DataFrame(columns=['DRG', 'nerve', 'subject','Threshold','Dynamic Range','Threshold (nC)','Dynamic Range (nC)','type','binarySearchRes'])
 nonSelectiveDF = pd.DataFrame(columns=['DRG', 'nerve', 'subject','type'])
 agonistDF = pd.DataFrame(columns=['DRG', 'nerve', 'subject','type'])
 
 numChan = {}
-coactivationDict_perDRG = {}
 for iDRG in hf.allDRG:
-    coactivationDF_perDRG = pd.DataFrame(0,columns=targetNerveLabels, index=targetNerveLabels)
 
     for subject in subjectList:
         numChan.setdefault(subject,{'selectiveElec':0, 'activeElec':0, })
@@ -38,22 +37,17 @@ for iDRG in hf.allDRG:
                 threshChans = sorted(threshDict.keys())
                 discardChans = hf.getSingleAmplitudeChannels(subject, iSesh)
                 allStimChans = [chans for chans in threshChans if chans not in discardChans]
+                binaryRes = hf.convertCurrentToCharge(hf.binarySearchResolutionbySession[subject][iSesh], subject,iSesh)
 
                 for iStimChan in allStimChans:
                     cuffThresholds = threshDict[iStimChan]
+                    maxAmp = hf.maxAmplitudePerSession(subject, iSesh)
 
                     # print cuffThresholds
                     allRecruitedCuffs = cuffThresholds.keys()
 
                     if cuffThresholds:
-                        # coactivation matrix
                         numChan[subject]['activeElec'] += 1
-                        threshAmp = min(cuffThresholds.values())
-                        coactivatedCuffs = [x for x in cuffThresholds.keys() if cuffThresholds[x] <= threshAmp]
-
-                        for iTargetCuff in coactivatedCuffs:
-                            for iCoactive in coactivatedCuffs:
-                                coactivationDF_perDRG.loc[iTargetCuff, iCoactive] += 1
 
                         # selectivity counts
                         cuffThresholds.pop("Sciatic_Proximal", None)
@@ -86,14 +80,14 @@ for iDRG in hf.allDRG:
 
                             if rangeList:
                                 dynamicRange = min(rangeList)
-                                dynamicRange_nC = hf.convertCurrentToCharge(thresh_nC, subject, iSesh)
                             else:
-                                dynamicRange = 0.0
-                                dynamicRange_nC = 0.0
+                                dynamicRange = maxAmp - threshAmp
+
+                            dynamicRange_nC = hf.convertCurrentToCharge(dynamicRange, subject, iSesh)
                             selectiveDF = selectiveDF.append(
                                                 {'DRG': iDRG, 'nerve': hf.allCuffs_mdf[selectiveCuffLabel], 'subject': subject,
                                                  'Threshold': threshAmp, 'Dynamic Range': dynamicRange,
-                                                 'Threshold (nC)': thresh_nC, 'Dynamic Range (nC)': dynamicRange_nC,
+                                                 'Threshold (nC)': thresh_nC, 'Dynamic Range (nC)': dynamicRange_nC, 'binarySearchRes':binaryRes,
                                                  'type': 'selective'
                                                  }, ignore_index=True)
 
@@ -120,97 +114,65 @@ for iDRG in hf.allDRG:
                     else:
                         print 'no results for this session'
 
-    coactivationDict_perDRG[iDRG] = coactivationDF_perDRG
-    coactivationDF += coactivationDF_perDRG
+print numChan
+pd.DataFrame.from_dict(numChan).transpose().to_csv(eType + '/selective_counts.csv')
+selectiveDF.to_csv(eType + '/selectiveDF.csv', index=False)
 
+# # selective, non selective and agonist counts
+concatData = selectiveDF.append(agonistDF).append(nonSelectiveDF, ignore_index=True)
+barOrder = [x for x in hf.allCuffs_mdf.values() if x in concatData['nerve'].unique()]
 
-
-# generate normalized figures
-tmp3 = coactivationDF.loc[:, (coactivationDF != 0).any(axis=0)]
-tmp4 = tmp3.loc[(tmp3 != 0).any(axis=1), :]
-activeNerves = tmp4.columns
-tmp4.index = [hf.allCuffs_mdf[i] for i in activeNerves]
-tmp4.columns = tmp4.index
-tmp4 = tmp4.div(tmp4.max(axis=1), axis=0)
-sns.heatmap(tmp4, annot=False, fmt=".1f",)
-plt.yticks(rotation=0)
-# plt.savefig(eType + '/allDRG_coactivation.pdf')
-# plt.savefig(eType + '/allDRG_coactivation.png')
-plt.close()
-
-for iDRG in hf.allDRG:
-    coactivationDF_perDRG = coactivationDict_perDRG[iDRG][activeNerves]
-    tmp2 = coactivationDict_perDRG[iDRG].loc[activeNerves, activeNerves]
-    tmp2.index = [hf.allCuffs_mdf[i] for i in tmp2.columns]
-    tmp2.columns = tmp2.index
-    tmp2 = tmp2.div(tmp2.max(axis=1), axis=0)
-
-    zm = np.ma.masked_less(tmp2.fillna(99).values,98)
-    x = np.arange(len(tmp2.columns) + 1)
-    y = np.arange(len(tmp2.index) + 1)
-    sns.heatmap(tmp2, annot=False, fmt=".1f", linewidths=.5)
-    plt.pcolor(x, y, zm, hatch='//', alpha=0.)
-    plt.yticks(rotation=0)
-    # plt.savefig(eType + '/' + iDRG + '_coactivation.pdf')
-    # plt.savefig(eType + '/' + iDRG + '_coactivation.png')
-    plt.close()
-
-
-
-# # generate non normalized figures
-# tmp3 = coactivationDF.loc[:, (coactivationDF != 0).any(axis=0)]
-# tmp4 = tmp3.loc[(tmp3 != 0).any(axis=1), :]
-# activeNerves = tmp4.columns
-# tmp4.index = [hf.allCuffs_mdf[i] for i in activeNerves]
-# tmp4.columns = tmp4.index
-# # tmp4 = tmp4.div(tmp4.max(axis=1), axis=0)
-# sns.heatmap(tmp4, annot=True, fmt="d",)
-# plt.yticks(rotation=0)
-# plt.savefig(eType + '/allDRG_coactivation_nonNorm.pdf')
-# plt.savefig(eType + '/allDRG_coactivation_nonNorm.png')
-# plt.close()
-#
-# for iDRG in hf.allDRG:
-#     coactivationDF_perDRG = coactivationDict_perDRG[iDRG][activeNerves]
-#     tmp2 = coactivationDict_perDRG[iDRG].loc[activeNerves, activeNerves]
-#     tmp2.index = [hf.allCuffs_mdf[i] for i in tmp2.columns]
-#     tmp2.columns = tmp2.index
-#     # tmp2 = tmp2.div(tmp2.max(axis=1), axis=0)
-#
-#     zm = np.ma.masked_less(tmp2.fillna(99).values,98)
-#     x = np.arange(len(tmp2.columns) + 1)
-#     y = np.arange(len(tmp2.index) + 1)
-#     sns.heatmap(tmp2, annot=True, fmt="d") # linewidths=.5
-#     plt.pcolor(x, y, zm, hatch='//', alpha=0.)
-#     plt.yticks(rotation=0)
-#     plt.savefig(eType + '/' + iDRG + '_coactivation_nonNorm.pdf')
-#     plt.savefig(eType + '/' + iDRG + '_coactivation_nonNorm.png')
-#     plt.close()
-
-
-# print numChan
-# pd.DataFrame.from_dict(numChan).transpose().to_csv(eType + '/selective_counts.csv')
-
-barOrder = [x for x in tmp4.index if x !='Fem' and x!= 'Sci']
-tmp5 = selectiveDF.append(agonistDF).append(nonSelectiveDF, ignore_index=True)
-f,ax3 = plt.subplots(2, 1, figsize=(9, 12))
-sns.countplot(x='nerve',hue='DRG',order=barOrder,data=selectiveDF, ax=ax3[0])
-# sns.countplot(x='nerve',order=tmp4.index,data=selectiveDF, ax=ax3[1])
-sns.countplot(x='nerve',hue='type',hue_order=['selective','non-selective','agonist'], order=barOrder,data=tmp5,ax=ax3[1])
-# sns.countplot(x='DRG',hue='subject',order=hf.allDRG,data=selectiveDF, ax=ax3[2])
+f,ax3 = plt.subplots(figsize=(9, 6))
+sns.countplot(x='nerve',hue='type',hue_order=['selective','non-selective','agonist'], order=barOrder,data=concatData)
 plt.savefig(eType + '/selectiveCounts.pdf')
 plt.savefig(eType + '/selectiveCounts.png')
 plt.close()
 
 
+# polar plot of selective recruitment
+data = []
+for iDRG in ['DRG - L5', 'DRG - L6', 'DRG - L7']:
+    nerveCounts = selectiveDF[selectiveDF['DRG'] == iDRG].nerve.value_counts()
+    polarVals = []
+    for iKey in barOrder:
+        if iKey in nerveCounts.keys():
+            polarVals.append(nerveCounts[iKey])
+        else:
+            polarVals.append(0)
 
+    data.append(
+        go.Scatterpolar(
+          name = iDRG,
+          r = polarVals,
+          theta = barOrder,
+          fill = "toself"
+        ),)
+
+layout = go.Layout(
+    polar = dict(
+      radialaxis = dict(
+        angle = 45
+      ),
+      angularaxis = dict(
+        direction = "clockwise",
+        period = 6
+      )
+    ),)
+fig = go.Figure(data=data, layout=layout)
+# plotly.offline.plot(fig, filename=eType+'\\polarSelectivity.html')
+plotly.plotly.image.save_as(fig, filename=eType+'\\polarSelectivity.pdf')
+plotly.plotly.image.save_as(fig, filename=eType+'\\polarSelectivity.png')
+
+
+
+# # threshold and DR distributions in uA
 DR = selectiveDF['Dynamic Range (nC)']
 DR = DR[DR>0]
 TH = selectiveDF['Threshold (nC)']
 maxVal_thresh = int(max(selectiveDF['Threshold (nC)']))
 if eType == 'penetrating':
     binSize_thresh = 0.3
-    binSize_DR = 0.05
+    binSize_DR = 0.15
 else:
     binSize_thresh = 3
     binSize_DR = 0.28
@@ -219,14 +181,22 @@ maxVal_DR = int(max(DR))
 
 f,ax4 = plt.subplots(1, 2, figsize=(12, 5))
 sns.distplot(TH, bins=np.arange(0, maxVal_thresh, binSize_thresh) , kde=False,ax=ax4[0]) # range(0,maxVal_thresh,stepSize_thresh)
+ax4[0].plot([np.median(TH), np.median(TH)], [0, 2], linewidth=2)
 ax4[0].text(0, 0, np.median(TH))
 sns.distplot(DR, bins=np.arange(0,maxVal_DR,binSize_DR), kde=False,ax=ax4[1])
+ax4[1].plot([np.median(DR), np.median(DR)], [0, 2], linewidth=2)
 ax4[1].text(0, 0, np.median(DR))
+
+TH.to_csv(eType + '/thresholds.csv',header=['vals'])
+DR.to_csv(eType + '/dynamicRanges.csv',header=['vals'])
+
 plt.savefig(eType + '/thresh_DR_charge.pdf')
 plt.savefig(eType + '/thresh_DR_charge.png')
 plt.close()
 
 
+
+# # threshold and DR distributions in uA
 # DR = selectiveDF['Dynamic Range']
 # DR = DR[DR>0]
 # TH = selectiveDF['Threshold']
@@ -243,10 +213,10 @@ plt.close()
 # plt.savefig(eType + '/thresh_DR_amplitude.png')
 # plt.close()
 
-
+# # non selective counts per animal
 # f,axes = plt.subplots(3, 1, figsize=(9, 12))
-# sns.countplot(x='nerve',hue='DRG',order=tmp4.index,data=nonSelectiveDF, ax=axes[0])
-# sns.countplot(x='nerve',order=tmp4.index,data=nonSelectiveDF, ax=axes[1])
+# sns.countplot(x='nerve',hue='DRG',order=barOrder,data=nonSelectiveDF, ax=axes[0])
+# sns.countplot(x='nerve',order=barOrder,data=nonSelectiveDF, ax=axes[1])
 # sns.countplot(x='DRG',hue='subject',order=hf.allDRG,data=nonSelectiveDF, ax=axes[2])
 # plt.savefig(eType + '/' + 'nonselectiveCounts.pdf')
 # plt.savefig(eType + '/' + 'nonselectiveCounts.png')
